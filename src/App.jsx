931f831b3769ed5b1adbfd187af569a2085c07fe
nova vita <42131370+josephc1417@users.noreply.github.com>
2023-09-06 11:19:19 -0400

    Establishing Application Structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,42 @@
+import {
+  Hero,
+    Services,
+    Nav,
+    PopularProducts,
+    SuperQuality,
+    SpecialOffers,
+    CustomerReviews,
+    Subscribe,
+    Footer,
+} from './sections/index'
+
 const App = () => (
   <main className="relative">
-    Nav
+    <Nav/>
     <section className="xl:padding-1 wide:padding-r padding-b">
-      Hero
+      <Hero/>
     </section>
-    <section className="xl:padding-1 wide:padding-r padding-b">
-      Hero
+    <section className="padding">
+     <PopularProducts/>
+    </section>
+    <section className="padding">
+     <SuperQuality/>
+    </section>
+    <section className="padding-x py-10">
+     <Services/>
+    </section>
+    <section className="padding">
+     <SpecialOffers/>
+    </section>
+    <section className="padding bg-pale-blue">
+     <CustomerReviews/>
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+     <Subscribe/>
+    </section>
+    <section className="bg-black padding-x padding-t pb-8
+    ">
+     <Footer/>
     </section>
   </main>
 )
---
+++ src/sections/index.js
@@ -0,0 +1,21 @@
+import Hero from './Hero'
+import Nav from './Nav'
+import PopularProducts from './PopularProducts'
+import SuperQuality from './SuperQuality'
+import Services from './Services'
+import SpecialOffers from './SpecialOffers'
+import CustomerReviews from './CustomerReview'
+import Subscribe from './Subscribe'
+import Footer from './Footer'
+
+export {
+    Hero,
+    Services,
+    Nav,
+    PopularProducts,
+    SuperQuality,
+    SpecialOffers,
+    CustomerReviews,
+    Subscribe,
+    Footer,
+}
