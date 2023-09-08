780e029d838f96f0d513e3de31f76f8294c96fd4
nova vita <42131370+josephc1417@users.noreply.github.com>
2023-09-08 07:41:32 -0400

    Completed PopularProducts Component

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -9,7 +9,7 @@ import { useState } from "react"
 const Hero = () => {
 const [bigShoeImg, setBigShoeImg] = useState(bigShoe1)
   return (
-    <section id="home" className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container border-2 border-red-500 p-2">
+    <section id="home" className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container  p-2">
       <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28" >
          <p className="text-xl font-montserrat text-coral-red ">Our Summer Collection</p>
          <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold">
--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -1,9 +1,31 @@
 
+import { products } from "../constants"
+import PopularProductCard from "../components/PopularProductCard"
+
+import { products } from "../constants";
+import PopularProductCard from "../components/PopularProductCard";
+
+
 
 const PopularProducts = () => {
   return (
-    <div>PopularProducts</div>
-  )
+    <section id="products" className="max-container max-sm:mt-12"> 
+    <div className="flex flex-col justify-start gap-5">
+    <h2 className="text-4xl font-palanquin font bold">Our<span className="text-coral-red">Popular</span>Products</h2>
+    <p className="lg:max-w-lg mt-2 front-montserrat text-slate-gray">Experience top-notch quality and style with the latest selection. Discover a world of comfort, design, and value.</p>
+      </div>
+      <div className="mt-16 grid:lg grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-4 gap-14">
+        {products.map((product) => {
+          return (
+            <PopularProductCard key={product.name} {...product}/>
+            /*each {product} of {products} thats mapped will return a {PopularProductCard-component} inside {Additional Component*/
+            /*{PopularProductCard} needs to be created/imported for use*/
+            /*Destructuring all kvp's from the {Products} obj as props*/
+            );
+        })}
+      </div>
+    </section>
+  );
 }
 
-export default PopularProducts
+export default PopularProducts;
