dc2adfb75b4a89a3efd5d160dd898925a071fc4c
nova vita <42131370+josephc1417@users.noreply.github.com>
2023-09-09 04:31:32 -0400

    Added custom color :) Still needs more work

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -9,13 +9,13 @@ import { useState } from "react"
 const Hero = () => {
 const [bigShoeImg, setBigShoeImg] = useState(bigShoe1)
   return (
-    <section id="home" className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container  p-2 ">
+    <section id="home" className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container">
       <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28" >
          <p className="text-xl font-montserrat text-coral-red ">Our Summer Collection</p>
          <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold">
-          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">The New Arrival</span>
+          <span className=" xl:whitespace-nowrap relative z-10 pr-10">The New </span>
           <br />
-          <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
+          <span className="text-coral-red inline-block mt-3"  style={{ textShadow: '10px 6px 4px black' }}>Arrival</span> Nike Shoes
          </h1>
          <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">Discover stylish Nike arrivals, quality comfort, 
           and innovation for your active life.
@@ -34,7 +34,7 @@ const [bigShoeImg, setBigShoeImg] = useState(bigShoe1)
       </div>
 
       <div className="relative flex-1 flex justify-center items-center
-      xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center rounded-2xl">
+      xl:min-h-screen max-xl:py-40  bg-hero bg-cover bg-center rounded-2xl">
         <img src={bigShoe1} alt="shoe collection" width={610} height={500}
         className=" bg-background object-contain relative z-10 border-r-orange-600 rounded-lg" />
       </div>
--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,8 +1,41 @@
+import { shoe8 } from "../assets/images"
+import Button from "../components/button"
 
 const SuperQuality = () => {
   return (
-    <div>SuperQuality</div>
+    <section id="about-us" className="flex justify-center items-center 
+    max-lg:flex-col gap-10 w-full max-container">
+    <div className="flex flex-1 flex-col">
+ 
+        <h2 className=" font-palanquin text-4xl capitalize font-bold lg:max-w-lg justify-center">
+        We Provide You
+          <br />
+          <span className="text-coral-red ">Super</span> 
+          <span className="text-coral-red "> Quality</span> Shoes 
+         </h2>
+         <p className="mt-4 lg:max-w-lg info-text">Ensuring premium comfort and style, our meticulously crafted
+         is designed to elevate your experience, providing you with unmatched quality,
+         innovation, and a touch of elegance.
+         </p>
+         <p className="mt-6  lg:max-w-lg info-text">
+         Our dedication to detail and excellence ensures your satisfaction
+         </p>
+         <div className="mt-11">
+         <Button label="Shop Now" />
+         </div>
+      </div>
+
+      <div className="flex-1 flex justify-center items">
+        <img className="object-contain"
+        src={shoe8}
+        alt="shoe8"
+        width={570}
+        height={522}
+        />
+      </div>
+    </section>
   )
 }
 
 export default SuperQuality
+
