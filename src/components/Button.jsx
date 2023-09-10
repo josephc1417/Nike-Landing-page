a5e39966caaad44d35661a72cda06153960a0a40
nova vita <42131370+josephc1417@users.noreply.github.com>
2023-09-10 07:39:05 -0400

    Sign-up Section Complete

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,26 @@
+/**
+ * Renders a button component with customizable properties such as label, icon URL, background color,
+ * text color, and border color.
+ * @param {string} label - The text to display on the button.
+ * @param {string} iconURL - The URL of the icon to display next to the label.
+ * @param {string} backgroundColor - The background color of the button.
+ * @param {string} textColor - The text color of the button.
+ * @param {string} borderColor - The border color of the button.
+ * @returns The rendered button component.
+ */
 
-const Button = ({label,iconURL, backgroundColor,textColor,borderColor}) => {
+
+
+
+const Button = ({label,iconURL, backgroundColor,textColor,borderColor, fullWidth}) => {
   return (
     //**Going to make the className Dynamic({`template String`})
     //**This will allow us to apply addition classNames depending on specific conditions :) Yey!! */
     //**Open up a new dynamic block of code>> use a Ternary conditional statement */
     //**${backgroundColor? `${backgroundColor} ${textColor} ${borderColor}:  bg-coral-red rounded-full text-white border-coral-red`} */
      <button className={`flex justify-center items-center gap-2 px-3 py-4 border font-montserrat text-lg leading-none ${backgroundColor ? 
-      `${backgroundColor} ${textColor} ${borderColor}` : "bg-coral-red rounded-full text-white border-coral-red"} rounded-full border-none`}>
+      `${backgroundColor} ${textColor} ${borderColor}` : "bg-coral-red rounded-full text-white border-coral-red"} rounded-full
+      ${fullWidth &&'w-full'} border-none`}>
 
         {label}
     {iconURL && <img src={iconURL} alt="arrow right icon" className="ml-2 rounded-full w-5 h-5" />}
--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -1,7 +1,29 @@
+/**
+ * Renders a subscription section with a sign-up form and a button.
+ * @returns {JSX.Element} - The rendered subscription section.
+ */
+
+
+
+import Button from "../components/Button"
 
 const Subscribe = () => {
   return (
-    <div>Subscribe</div>
+    <section className="max-container flex justify-center items-center 
+    max-lg:flex-col  gap-10"  id="contact-us">
+      <h3 className=" flex justify-center items-center flex-col text-4xl leading-[68px] lg:max-w-md font-palanquin font-bold">Sign Up from
+      <span className="text-coral-red">  Updates </span> & Newsletter
+      </h3>
+
+      <div className="lg:max-w-[40%] w-full
+      flex items-center max-sm:flex-col gap-5 p-2.5
+      sm:border sm:border-slate-gray rounded-full bg-white" style={{boxShadow:'5px 10px 35px black'}}>
+        <input className="input" type="text" placeholder="Subscribe@nike.com"/>
+        <div className="flex max-sm:justify-end items-center max-sm:w-full">
+          <Button label="Sign Up" fullWidth/>
+        </div>
+      </div>
+    </section>
   )
 }
 
