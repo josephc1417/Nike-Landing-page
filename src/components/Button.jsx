94efffbd2e8a4fa615c28605737ff2604f5b0d41
nova vita <42131370+josephc1417@users.noreply.github.com>
2023-09-10 03:16:52 -0400

    Button Configuration

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,8 +5,9 @@ const Button = ({label,iconURL, backgroundColor,textColor,borderColor}) => {
     //**This will allow us to apply addition classNames depending on specific conditions :) Yey!! */
     //**Open up a new dynamic block of code>> use a Ternary conditional statement */
     //**${backgroundColor? `${backgroundColor} ${textColor} ${borderColor}:  bg-coral-red rounded-full text-white border-coral-red`} */
-    <button className="{`flex justify-center items-center gap-2 px-7 py-4 border 
-    font-montserrat text-lg leading-none ${backgroundColor? `${backgroundColor} ${textColor} ${borderColor}: bg-coral-red rounded-full text-white border-coral-red`}">
+     <button className={`flex justify-center items-center gap-2 px-3 py-4 border font-montserrat text-lg leading-none ${backgroundColor ? 
+      `${backgroundColor} ${textColor} ${borderColor}` : "bg-coral-red rounded-full text-white border-coral-red"} rounded-full `}>
+
         {label}
     {iconURL && <img src={iconURL} alt="arrow right icon" className="ml-2 rounded-full w-5 h-5" />}
     </button>
--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -20,7 +20,7 @@ const SuperQuality = () => {
          <p className="mt-6  lg:max-w-lg info-text">
          Our dedication to detail and excellence ensures your satisfaction
          </p>
-         <div className="mt-11">
+         <div className="mt-11 ">
          <Button label="Shop Now" />
          </div>
       </div>
