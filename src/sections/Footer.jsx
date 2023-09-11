b9dd6de570c2c8afbadf4385c799742c3c0ea213
nova vita <42131370+josephc1417@users.noreply.github.com>
2023-09-10 22:43:07 -0400

    Footer Completed

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,8 +1,78 @@
-
+import { copyrightSign } from "../assets/icons"
+import { footerLogo } from "../assets/images"
+import { footerLinks, socialMedia } from "../constants"
 
 const footer = () => {
   return (
-    <div>footer</div>
+    <footer className="max-container">
+    <div className="flex justify-between items-start gap-20 flex-wrap max-lg:flex-col ">
+      <div className="flex flex-col">
+      <a href="/">
+      <img src={footerLogo}
+      width={150}
+      height={146}/>
+      </a>
+      <p class="mt-6 text-base leading-7 font-montserrat
+      text-white-400 sm:max-w-sm">
+      Get shoes ready for the new term ar your
+      nearest Nike store. Find Your Perfect Size In 
+      Store. Get Rewards.
+      </p>
+      <div className="flex items-center gap-5 mt-8">
+      {socialMedia.map((icon) => {
+        return(
+          <div className="flex justify-center items-center w-12 h-12 bg-white rounded-full">
+          <img src={icon.src}
+          alt={icon.alt}
+          width={24}
+          height={24}
+          />
+          </div>
+        )
+      })}  
+       </div>
+      </div>
+
+      <div className="flex flex-1 justify-between lg:gap-10 gap-20 flex-wrap">
+      {footerLinks.map((section) => {
+        return(
+         <div key={section}>
+          <h4 className="text-white">
+          {section.title}
+          </h4>
+          <ul>
+            {section.links.map((link) => {
+              return(
+                <li className="mt-3 text-white-400 font-montserrat text-base
+                leading-normal hover:text-slate-gray cursor-pointer" key={link.name}>
+                <a>{link.name}</a>
+                </li>
+              )
+            })}
+          </ul>
+         </div>
+        )
+      })}
+    
+      </div>
+    
+      </div>
+      <div className="flex justify-between text-white-400 mt-24 max-sm:flex-col max-sm:items-center">
+        <div className="flex flex-1 justify-start items center gap-2 font-montserrat cursor-pointer">
+          <img className="rounded-full m-0"
+          src={copyrightSign}
+          width={24}
+          height={20}
+          />
+          <p className="">
+          Copyright. All right reserved
+          </p>
+        </div>
+        <p className="font-montserrat cursor-pointer">
+        Terms & Conditions
+        </p>
+      </div>
+    </footer>
   )
 }
 
